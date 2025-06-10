262b98641394966d9db4e316cbea2fa7ba946c90
jjdishere <emailboxofjjd@163.com>
2025-06-11 00:13:37 +0800

    init w-local space

---
+++ Proetale/Topology/SpectralSpace/WLocal.lean
@@ -0,0 +1,22 @@
+/-
+Copyright (c) 2025 Jiedong Jiang. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Jiedong Jiang
+-/
+import Mathlib.Topology.Spectral.Hom
+import Mathlib.Topology.JacobsonSpace
+
+-- universe problem, index set of "all open covers" lives in the same universe as the space
+universe u
+
+class WLocalSpace (X : Type u) [TopologicalSpace X] : Prop where
+  open_cover_splits {I : Type u} {U : I → Set X} (ho : ∀ (i : I), IsOpen (U i)) (hU : ⋃ (i : I), U i = Set.univ) : ∃ s : C(X, (i : I) × U i), ∀ (i : I) (u : U i), s u = ⟨i, u⟩
+  isClosed_closedPoints : IsClosed (closedPoints X)
+
+structure WLocalMap (X Y : Type*) [TopologicalSpace X] [TopologicalSpace Y] extends SpectralMap X Y where
+  image_closedPoints' : toFun '' (closedPoints X) ⊆ closedPoints Y
+
+-- WLocalMapClass
+
+
+def WLocalSpace.closedPointsHomeomorph {X : Type*} [TopologicalSpace X] [WLocalSpace X] : closedPoints X ≃ₜ ConnectedComponents X := sorry
