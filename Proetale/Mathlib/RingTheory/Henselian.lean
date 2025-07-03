7424fc1a9aae38b498280760895ee1dc3fa0a25a
jjdishere <emailboxofjjd@163.com>
2025-07-03 13:24:33 +0900

    bump

--- Proetale/Mathlib/RingTheory/Henselian.lean
+++ Proetale/Mathlib/RingTheory/Henselian.lean
@@ -13,13 +13,14 @@ instance (R : Type*) [CommRing R] [IsStrictlyHenselianLocalRing R] :
 universe u v
 
 noncomputable section
-open Polynomial Ideal Quotient Algebra
+open Polynomial
+open MvPolynomial Ideal Quotient Algebra
 
 variable {R : Type u} [CommRing R] (f : R[X])
 
 -- f(X), f'(X)Y - 1
 private def idealJ (f : R[X]) : Ideal (MvPolynomial (Fin 2) R) :=
-  (span {toMvPolynomial (0 : Fin 2) f, (toMvPolynomial (0 : Fin 2) f.derivative) * MvPolynomial.X 1 - 1})
+  (span {toMvPolynomial (0 : Fin 2) f, (toMvPolynomial (0 : Fin 2) f.derivative) * X 1 - 1})
 
 private def S : Type u := MvPolynomial (Fin 2) R ⧸ (idealJ f)
 
@@ -54,7 +55,7 @@ private theorem aeval_zero_of_mem_span {I : Ideal R} {f : R[X]} {a₀ : R} (e : 
     (u : IsUnit ((Ideal.Quotient.mk I) (Polynomial.eval a₀ (derivative f)))) :
     ∀ a ∈ idealJ f,
     (MvPolynomial.aeval
-    ![(Ideal.Quotient.mk I) a₀, (Ideal.Quotient.mk I) ((aeval a₀) (derivative f))]) a = 0 := by
+    ![(Ideal.Quotient.mk I) a₀, (Ideal.Quotient.mk I) ((derivative f).aeval a₀)]) a = 0 := by
   sorry
 
 private def g {I : Ideal R} {f : R[X]} {a₀ : R} (e : Polynomial.eval a₀ f ∈ I)
@@ -70,7 +71,7 @@ theorem henselian_if_exists_section (R : Type u)
       is_henselian := by
           intro f monic a₀ e u
           obtain ⟨σ, hσ⟩ := h (S f) (g e u)
-          use σ (mk _ (MvPolynomial.X 0))
+          use σ (mk _ (X 0))
           constructor
           · sorry -- f (X_0) = 0 since kernel contains f(X_0)
           · sorry -- σ (X_0) = a₀ since σ is a section of the quotient map (hσ)
