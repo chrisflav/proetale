a649601a1a6b204854d70f381891960d138718bc
Jiedong Jiang <emailboxofjjd@163.com>
2025-11-12 22:09:46 +0800

    finish topological part in preliminary

--- Proetale/Topology/Preliminaries/Pullback.lean
+++ Proetale/Topology/Preliminaries/Pullback.lean
@@ -3,16 +3,22 @@ Copyright (c) 2025 Jiedong Jiang, Christian Merten. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Jiedong Jiang, Christian Merten
 -/
+import Mathlib.CategoryTheory.Limits.Shapes.Pullback.CommSq
 import Proetale.Mathlib.Topology.Category.TopCat.Limits.Pullbacks
 import Proetale.Mathlib.Topology.Spectral.Basic
 
 universe u
 
-variable {X Y S : Type u} [TopologicalSpace X] [TopologicalSpace Y] [TopologicalSpace S]
-
 open CategoryTheory TopCat Limits
 
-instance T2Space.pullback [T2Space X] [T2Space Y] (f : C(X, S)) (g : C(Y, S)) :
+variable {X Y S : Type u} [TopologicalSpace X] [TopologicalSpace Y] [TopologicalSpace S]
+variable (f : C(X, S)) (g : C(Y, S))
+
+variable {P : Type u} [TopologicalSpace P] {f' : C(P, Y)} {g' : C(P, X)} (pb : IsPullback (ofHom g') (ofHom f') (ofHom f) (ofHom g))
+
+section pullback
+
+instance T2Space.pullback [T2Space X] [T2Space Y]  :
     T2Space (pullback (ofHom f) (ofHom g) : TopCat) := (homeoOfIso (pullbackIsoProdSubtype (ofHom f) (ofHom g))).symm.t2Space
 
 instance T0Space.pullback [T0Space X] [T0Space Y] (f : C(X, S)) (g : C(Y, S)) :
@@ -43,3 +49,35 @@ instance SpectralSpace.pullback [SpectralSpace X] [SpectralSpace Y] (f : C(X, S)
   let _ := (homeoOfIso (prodIsoProd (of X) (of Y))).symm.spectralSpace
   (isClosedEmbedding_pullback_to_prod (ofHom f) (ofHom g)).spectralSpace
 
+end pullback
+
+section IsPullback
+
+variable {f g}
+
+include f' g' pb
+
+theorem CategoryTheory.IsPullback.t2Space [T2Space X] [T2Space Y] : T2Space P :=
+  (homeoOfIso pb.isoPullback).symm.t2Space
+
+theorem CategoryTheory.IsPullback.t0Space [T0Space X] [T0Space Y] : T0Space P :=
+  (homeoOfIso pb.isoPullback).symm.t0Space
+
+theorem CategoryTheory.IsPullback.quasiSober [QuasiSober X] [QuasiSober Y] : QuasiSober P :=
+  (homeoOfIso pb.isoPullback).symm.quasiSober
+
+theorem CategoryTheory.IsPullback.quasiSeparatedSpace [QuasiSeparatedSpace X] [QuasiSeparatedSpace Y] :
+    QuasiSeparatedSpace P :=
+  (homeoOfIso pb.isoPullback).symm.quasiSeparatedSpace
+
+theorem CategoryTheory.IsPullback.compactSpace [CompactSpace X] [CompactSpace Y] : CompactSpace P :=
+  (homeoOfIso pb.isoPullback).symm.compactSpace
+
+theorem CategoryTheory.IsPullback.prespectralSpace [PrespectralSpace X] [PrespectralSpace Y] :
+    PrespectralSpace P :=
+  (homeoOfIso pb.isoPullback).symm.prespectralSpace
+
+theorem CategoryTheory.IsPullback.spectralSpace [SpectralSpace X] [SpectralSpace Y] : SpectralSpace P :=
+  (homeoOfIso pb.isoPullback).symm.spectralSpace
+
+end IsPullback
--- Proetale/Topology/SpectralSpace/ConnectedComponent.lean
+++ Proetale/Topology/SpectralSpace/ConnectedComponent.lean
@@ -4,6 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Jiedong Jiang, Christian Merten
 -/
 import Proetale.Topology.Preliminaries.ConnectedComponent
+import Proetale.Topology.Preliminaries.Pullback
 import Mathlib.Topology.Spectral.Basic
 import Proetale.Mathlib.Topology.Connected.TotallyDisconnected
 import Mathlib.CategoryTheory.Limits.Shapes.Pullback.CommSq
@@ -33,21 +34,21 @@ theorem sInter_isClopen_and_mem_eq_connectedComponent {x : X} :
   · exact connectedComponent_subset_iInter_isClopen
 
 @[stacks 04PL]
-theorem isClosed_and_iUnion_connectedComponent_iff {T : Set X} :
+theorem isClosed_and_iUnion_connectedComponent_eq_iff {T : Set X} :
     (IsClosed T ∧ ∃ I : Set X, ⋃ (x : I), connectedComponent x = T) ↔
     ∃ J : Set ({U : Set X // IsClopen U}), ⋂ (U : J), U = T := by
   sorry
   -- uses `ConnectedComponents.injective_lift`
 
-@[stacks 0906]
-instance compactSpace_connectedComponent {X : Type u} [TopologicalSpace X] [CompactSpace X]
-    [QuasiSeparatedSpace X] [PrespectralSpace X] : CompactSpace (ConnectedComponents X) :=
+instance compactSpace_connectedComponent {X : Type u} [TopologicalSpace X] [CompactSpace X] :
+    CompactSpace (ConnectedComponents X) :=
   sorry
 
 @[stacks 0906]
 instance t2Space_connectedComponent {X : Type u} [TopologicalSpace X]  [CompactSpace X]
     [QuasiSeparatedSpace X] [PrespectralSpace X] : T2Space (ConnectedComponents X) :=
   sorry
+  -- use `isClosed_and_iUnion_connectedComponent_eq_iff`
 
 end
 
@@ -55,22 +56,22 @@ section Spectral
 
 variable [SpectralSpace X]
 
-#check ConnectedComponents.lift
 open CategoryTheory TopCat ConnectedComponents
 
-theorem spectralSpace_of_isPullback_mk (Y T : Type u) [TopologicalSpace Y]
+theorem ConnectedComponents.lift_bijective_of_isPullback {Y T : Type u} [TopologicalSpace Y]
     [TopologicalSpace T] [CompactSpace T] [T2Space T] [TotallyDisconnectedSpace T]
-    (f : C(Y, X)) (g : C(Y, T)) (i : C(T, ConnectedComponents X))
+    {f : C(Y, X)} {g : C(Y, T)} {i : C(T, ConnectedComponents X)}
     (pb : IsPullback (ofHom g) (ofHom f) (ofHom i) (ofHom ⟨mk, continuous_coe⟩)) :
-    SpectralSpace Y := sorry
+    Function.Bijective (ConnectedComponents.lift g) := sorry
 
--- TBModified
 @[stacks 096C "first part"]
-theorem spectralSpace_of_isPullback_mk (Y T : Type u) [TopologicalSpace Y]
+theorem ConnectedComponents.isHomeomorph_lift_of_isPullback (Y T : Type u) [TopologicalSpace Y]
     [TopologicalSpace T] [CompactSpace T] [T2Space T] [TotallyDisconnectedSpace T]
     (f : C(Y, X)) (g : C(Y, T)) (i : C(T, ConnectedComponents X))
     (pb : IsPullback (ofHom g) (ofHom f) (ofHom i) (ofHom ⟨mk, continuous_coe⟩)) :
-    SpectralSpace Y := sorry
+    IsHomeomorph (ConnectedComponents.lift g) :=
+  let _ := IsPullback.compactSpace pb
+  (isHomeomorph_iff_continuous_bijective (X := ConnectedComponents Y) (Y := T)).mpr ⟨(lift g).2, lift_bijective_of_isPullback pb⟩
 
 -- open Profinite
 
