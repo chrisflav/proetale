dc03ada224517a23aa65d91c2749b55a6ba66aa0
xyzw12345 <wangjt2020@163.com>
2026-03-28 17:04:01 +0800

    add IsColimit result for ResidueField

--- Proetale/Algebra/FilteredLocalColimit.lean
+++ Proetale/Algebra/FilteredLocalColimit.lean
@@ -84,19 +84,64 @@ lemma maximalIdeal_eq_iSup_of_isColimit (hc : IsColimit c) :
   · have : IsLocalRing (((Functor.const J).obj c.pt).obj j) := isLocalRing_of_isColimit F hc
     exact ((IsLocalRing.local_hom_TFAE (c.ι.app j).hom).out 0 2).mp (isLocalHom_ι F hc j)
 
--- Is it possible to make `Functor.const.obj` reducible somehow? Is it possible to remove `let`?
 set_option linter.unusedVariables false in
-lemma residueField_eq_iUnion_of_isColimit (hc : IsColimit c) :
+lemma residueField_exists_rep (hc : IsColimit c) :
+    letI inst_isLocalRing := isLocalRing_of_isColimit F hc
+    letI inst_isLocalHom := isLocalHom_ι F hc
+    letI (j : J) : IsLocalRing (((Functor.const J).obj c.pt).obj j) := inst_isLocalRing
+    ∀ (x : ResidueField c.pt), ∃ (j : J) (y : ResidueField (F.obj j)), x = ResidueField.map (c.ι.app j).hom y:= by
+  intro x
+  obtain ⟨y, rfl⟩ := Ideal.Quotient.mk_surjective x
+  obtain ⟨j, z, rfl⟩ := Concrete.isColimit_exists_rep F hc y
+  exact ⟨j, residue _ z, rfl⟩
+
+set_option linter.unusedVariables false in
+lemma residueField_eq_iUnion_fieldRange_of_isColimit (hc : IsColimit c) :
     letI inst_isLocalRing := isLocalRing_of_isColimit F hc
     letI inst_isLocalHom := isLocalHom_ι F hc
     letI (j : J) : IsLocalRing (((Functor.const J).obj c.pt).obj j) := inst_isLocalRing
     ⋃ (j : J), (ResidueField.map (c.ι.app j).hom).fieldRange.carrier = (⊤ : Set (ResidueField c.pt)):= by
   ext x
-  obtain ⟨y, rfl⟩ := Ideal.Quotient.mk_surjective x
-  obtain ⟨j, z, rfl⟩ := Concrete.isColimit_exists_rep F hc y
+  obtain ⟨j, y, rfl⟩ := residueField_exists_rep F hc x
   simp only [Functor.const_obj_obj, Subsemiring.coe_carrier_toSubmonoid, Subring.coe_toSubsemiring,
     Subfield.coe_toSubring, RingHom.coe_fieldRange, Set.mem_iUnion, Set.mem_range, Set.top_eq_univ,
     Set.mem_univ, iff_true]
-  exact ⟨j, residue _ z, rfl⟩
+  exact ⟨j, y, rfl⟩
+
+/-- The functor of taking residue fields from a functor `F : J ⥤ CommRingCat`, when the `F.obj` are local
+  rings and `F.map` are local ring homomorphisms. -/
+noncomputable def residueFieldFunctor : J ⥤ CommRingCat.{u} where
+  obj j := CommRingCat.of <| ResidueField (F.obj j)
+  map f := CommRingCat.ofHom <| ResidueField.map (F.map f).hom
+
+/-- The cocone constructed from a filtered colimit cocone of local homomorphisms between local rings. -/
+noncomputable def residueFieldCocone (hc : IsColimit c) : Cocone (residueFieldFunctor F) :=
+  letI := isLocalRing_of_isColimit F hc
+  letI := isLocalHom_ι F hc
+  {
+    pt := CommRingCat.of <| ResidueField c.pt
+    ι := {
+      app j := CommRingCat.ofHom <| ResidueField.map (c.ι.app j).hom
+      naturality j j' f := by
+        simp [residueFieldFunctor, ← ofHom_comp, ← ResidueField.map_comp, ← hom_comp]
+    }
+  }
+
+instance : ReflectsFilteredColimits (forget CommRingCat.{u}) where
+  reflects_filtered_colimits _ := {reflectsColimit := reflectsColimit_of_reflectsIsomorphisms _ _}
+
+/-- For a filtered colimit cocone `c` of local homomorphisms between local rings, the `residueFieldCocone`
+  constructed from `c` is also a colimit cocone, i.e. the residue field of colimit of local rings
+  (and local homomorphisms) is a colimit of the residue field of these local rings. -/
+noncomputable def residueFieldCocone_isColimit (hc : IsColimit c) :
+    IsColimit (residueFieldCocone F hc) :=
+  letI (j : J) : Field ((residueFieldFunctor F).obj j) := inferInstanceAs <| Field (ResidueField _)
+  letI := isLocalRing_of_isColimit F hc
+  letI (j : J) : Nontrivial (((Functor.const J).obj (residueFieldCocone F hc).pt).obj j) :=
+    inferInstanceAs <| Nontrivial (ResidueField _)
+  isColimitOfReflects (forget CommRingCat.{u}) <| Types.FilteredColimit.isColimitOf' _ _
+  (fun x ↦ residueField_exists_rep F hc x)
+  (fun i x y hxy ↦ ⟨i, 𝟙 i, ((residueFieldCocone F hc).ι.app i).hom.injective (by simpa)⟩)
+
 
 end CommRingCat.FilteredColimits
