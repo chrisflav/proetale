fb451b4e58e08160d2c79cc154f352f0214065d8
xyzw12345 <wangjt2020@163.com>
2026-03-30 00:48:34 +0800

    proved Module.Flat.of_flat_lmul'_of_flat by chasing elements

--- Proetale/Algebra/WeakDimension.lean
+++ Proetale/Algebra/WeakDimension.lean
@@ -4,6 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Christian Merten
 -/
 import Proetale.Algebra.WeaklyEtale
+import Mathlib
 
 /-!
 # Weak dimension of a commutative ring
@@ -18,6 +19,13 @@ class Ring.AbsolutelyFlat (R : Type*) [CommRing R] where
 /-- A ring `R` is of weak dimension `≤ 1` if any finitely generated ideal is flat. -/
 class Ring.WeakDimensionLEOne (R : Type*) [CommRing R] where
   flat_of_fg (I : Ideal R) : I.FG → Module.Flat R I
+
+-- Follows from `Ideal.exists_eq_mul_of_pure` in mathlib
+lemma exists_eq_mul_of_surjective_flat {R S : Type*} [CommRing R] [CommRing S]
+    (f : R →+* S) (hf : f.Flat) (x : R) (hx : f x = 0) : ∃ y : R, f y = 1 ∧ y * x = 0 := sorry
+
+lemma exists_eq_mul_of_surjective_flat' {R S ι : Type*} [CommRing R] [CommRing S] [Finite ι]
+    (f : R →+* S) (hf : f.Flat) (x : ι → R) (hx : ∀ i, f (x i) = 0) : ∃ y : R, f y = 1 ∧ ∀ i : ι, y * x i = 0 := sorry
 
 namespace Ring.WeakDimensionLEOne
 
@@ -42,13 +50,36 @@ variable (R S M : Type*) [CommRing R] [CommRing S] [Algebra R S]
     [AddCommGroup M] [Module R M] [Module S M] [IsScalarTower R S M]
     (h : (Algebra.TensorProduct.lmul' (S := S) R).Flat)
 
+open TensorProduct
+
 include h in
 @[stacks 092C]
 theorem _root_.Module.Flat.of_flat_lmul'_of_flat [Module.Flat R M] : Module.Flat S M := by
-  constructor
-  rintro P _ _ _ N h
-
-  sorry
+  refine Module.Flat.of_forall_isTrivialRelation fun {l f x} hx ↦ ?_
+  obtain ⟨t, ht, ht'⟩ := exists_eq_mul_of_surjective_flat' _ h
+    (fun i : Fin l ↦ (1 : S) ⊗ₜ[R] (f i) - (f i) ⊗ₜ[R] (1 : S)) (fun i ↦ by simp)
+  obtain ⟨s, rfl⟩ := TensorProduct.exists_finset t
+  simp only [AlgHom.toRingHom_eq_coe, map_sum, RingHom.coe_coe,
+    Algebra.TensorProduct.lmul'_apply_tmul] at ht
+  simp only [Finset.sum_mul, mul_sub, Algebra.TensorProduct.tmul_mul_tmul, sub_eq_zero] at ht'
+  let φ (i : Fin l) : S ⊗[R] S →ₗ[R] S ⊗[R] M := LinearMap.lTensor S (LinearMap.toSpanSingleton S M (x i))
+  have hg : ∑ y : s ×ˢ Finset.univ, (y.1.1.1 * f y.1.2) ⊗ₜ[R] (y.1.1.2 • x y.1.2) = 0 := by
+    simp only [Finset.sum_coe_sort _ (fun y : (S × S) × Fin l ↦ (y.1.1 * f y.2) ⊗ₜ[R] (y.1.2 • x y.2)),
+      Finset.sum_product, Finset.sum_comm (s := s)]
+    replace ht' := by simpa [φ] using congr(fun i ↦ (φ i) $(ht' i))
+    simp only [← ht', Finset.sum_comm (t := s), ← tmul_sum, mul_smul, ← Finset.smul_sum, hx,
+      smul_zero, tmul_zero, Finset.sum_const_zero]
+  obtain ⟨l', g, y, ha, ha'⟩ := vanishesTrivially_of_sum_tmul_eq_zero_of_rTensor_injective R
+    (Module.Flat.rTensor_preserves_injective_linearMap _ (Submodule.subtype_injective _)) hg
+  refine ⟨l', fun i j ↦ s.attach.sum fun x ↦ x.1.1 * (algebraMap R S) (g ⟨⟨x.1, i⟩, by simp⟩ j),
+    y, fun i ↦ ?_, fun j ↦ ?_⟩
+  · simp only [Finset.sum_smul, mul_smul, Finset.sum_comm, ← Finset.smul_sum, algebraMap_smul]
+    simp only [← ha, ← mul_smul, ← Finset.sum_smul, ← Finset.sum_coe_sort_eq_attach,
+      Finset.sum_coe_sort _ fun (i : S × S) ↦ i.1 * i.2, ht, one_smul]
+  · simp only [Finset.mul_sum, ← ha' j, ← Finset.sum_product_right']
+    exact Finset.sum_bij' (fun a _ ↦ ⟨⟨a.1, a.2⟩, by simp⟩) (fun a _ ↦ ⟨⟨a.1.1, by grind⟩, a.1.2⟩)
+      (by simp) (by simp) (by simp) (by simp)
+      (by simp_rw [← mul_assoc, mul_comm (f _), mul_comm (_ * _), Algebra.smul_def, forall_true_iff])
 
 include h in
 @[stacks 092I "(2)"]
