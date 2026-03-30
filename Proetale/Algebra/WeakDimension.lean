1a298ccbf2241c737906b7e059c1a9a60a92a887
xyzw12345 <wangjt2020@163.com>
2026-03-30 21:40:20 +0800

    update copyright (?)

--- Proetale/Algebra/WeakDimension.lean
+++ Proetale/Algebra/WeakDimension.lean
@@ -1,7 +1,7 @@
 /-
-Copyright (c) 2026 Christian Merten. All rights reserved.
+Copyright (c) 2026 Christian Merten, Jingting Wang. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Christian Merten
+Authors: Christian Merten, Jingting Wang
 -/
 import Proetale.Algebra.WeaklyEtale
 import Mathlib
