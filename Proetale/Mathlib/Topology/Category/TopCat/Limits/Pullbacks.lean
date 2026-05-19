dace7e1ced3621daa1cb731ce1b015bbf44384e4
haowen214 <zhhhhw@gmail.com>
2026-05-19 14:26:02 +0200

    TopCatLemma [stacks 08ZH] (#33)
    Finished the proof of [stacks 08ZH] (isClosedEmbedding_pullback_to_prod)

--- Proetale/Mathlib/Topology/Category/TopCat/Limits/Pullbacks.lean
+++ Proetale/Mathlib/Topology/Category/TopCat/Limits/Pullbacks.lean
@@ -5,10 +5,37 @@ universe u
 
 open CategoryTheory Limits
 
+lemma TopCat.prodFst_apply {X Y : TopCat.{u}} (x : X × Y) : TopCat.prodFst x = x.1 := by
+  rfl
+
+lemma TopCat.prodSnd_apply {X Y : TopCat.{u}} (x : X × Y) : TopCat.prodSnd x = x.2 := by
+  rfl
+
 -- use the following lemma: `t2Space_iff_diagonal_closed`
 -- after `TopCat.isEmbedding_pullback_to_prod`
 @[stacks 08ZH]
 theorem TopCat.isClosedEmbedding_pullback_to_prod {X Y Z : TopCat.{u}} [T2Space Z]
     (f : X ⟶ Z) (g : Y ⟶ Z) :
-    Topology.IsClosedEmbedding ⇑(prod.lift (pullback.fst f g) (pullback.snd f g)) :=
-  sorry
+    Topology.IsClosedEmbedding ⇑(prod.lift (pullback.fst f g) (pullback.snd f g)) := by
+  refine ⟨isEmbedding_pullback_to_prod f g, ?_⟩
+  suffices s1 : Set.range (prod.lift (pullback.fst f g) (pullback.snd f g)) =
+      (homeoOfIso (prodIsoProd X Y)) ⁻¹' (Prod.map f.hom g.hom ⁻¹' Set.range fun x ↦ (x, x)) by
+    rw [s1, (TopCat.homeoOfIso (TopCat.prodIsoProd X Y)).isClosed_preimage]
+    exact .preimage (.prodMap (map_continuous f.hom) (map_continuous g.hom))
+      (t2Space_iff_diagonal_closed.mp ‹T2Space Z›)
+  ext x
+  simp only [Set.mem_range, Set.range_diag, Set.mem_preimage, Set.mem_diagonal_iff, Prod.map_fst,
+    Prod.map_snd]
+  constructor
+  · rintro ⟨a, rfl⟩
+    rw [← TopCat.prodFst_apply, ← TopCat.prodSnd_apply, homeoOfIso_apply]
+    simp only [← ConcreteCategory.comp_apply]
+    simp [pullback.condition]
+  · intro ha
+    -- TODO: replace this by `range_pullback_to_prod` after updating mathlib
+    use (pullbackIsoProdSubtype f g).inv ⟨(X.prodIsoProd Y).hom x , ha⟩
+    apply Concrete.limit_ext
+    rintro ⟨⟨⟩⟩ <;>
+       rw [← ConcreteCategory.comp_apply, ← ConcreteCategory.comp_apply, limit.lift_π] <;>
+        -- This used to be `simp` before https://github.com/leanprover/lean4/pull/2644
+    cat_disch
