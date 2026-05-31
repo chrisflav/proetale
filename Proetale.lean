25f20a42cde3d7ebad0c92e830b4253d7df562d8
chrisflav <christian@merten.dev>
2026-05-31 10:44:42 +0000

    chore: add `PrecoverageGenerating` to root file
    Required by `lake exe mk_all --check`.

--- Proetale.lean
+++ Proetale.lean
@@ -70,6 +70,7 @@ import Proetale.Mathlib.CategoryTheory.Sites.InducedTopology
 import Proetale.Mathlib.CategoryTheory.Sites.IsSheafFor
 import Proetale.Mathlib.CategoryTheory.Sites.MorphismProperty
 import Proetale.Mathlib.CategoryTheory.Sites.Precoverage
+import Proetale.Mathlib.CategoryTheory.Sites.PrecoverageGenerating
 import Proetale.Mathlib.CategoryTheory.Sites.Sieves
 import Proetale.Mathlib.FieldTheory.IsSepClosed
 import Proetale.Mathlib.Order.BooleanAlgebra.Set
